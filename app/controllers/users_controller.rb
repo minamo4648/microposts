9a98446441043070b6b301c1a1d54450845b7169
TakashiKawano <minamo.kawano@nifty.com>
2016-01-07 08:43:48 +0000

    Profile editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
 
+# users_controller.rb
+
+  def update
+    
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      flash[:success] = "Update succeeded"
+      redirect_to user_path
+    else
+      render 'edit'
+    end
+    
+  end
+
   def show # 追加
    @user = User.find(params[:id])
   end
@@ -11,17 +26,24 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :region, :profile)
   end
+  
 
 end
